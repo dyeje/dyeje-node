a4468e7f982f67bb18d2102d4d650b2d4dfb476a
Jeremy Dye <dyeje@mail.gvsu.edu>
2015-11-22 22:50:28 -0500

    CSS

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var express = require('express')
 var app = express()
 
 app.set('view engine', 'jade');
- 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
   res.render('index.jade', {})
 })
