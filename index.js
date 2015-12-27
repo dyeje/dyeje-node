f9b20826337bc8b6afca3f697ae92dbebe903737
Jeremy Dye <dyeje@mail.gvsu.edu>
2015-12-27 18:08:11 -0500

    Navigation working
    -DRY'd up the markup
    -Navigation links

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ app.set('view engine', 'jade');
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.render('index.jade', {})
+  res.render('about.jade', {
+    about: true
+  })
+})
+
+app.get('/portfolio', function (req, res) {
+  res.render('portfolio.jade', {
+    portfolio: true
+  })
 })
  
 app.listen(3000)
